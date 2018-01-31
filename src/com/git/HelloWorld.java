7ad97f257c598497315afb477e4f60ad2bd94ebd
prashant <prashant@prashant-Lenovo-G50-80>
2018-01-31 10:49:53 +0530

    Importing Project From GitHub Repo.

--- src/com/git/HelloWorld.java
+++ src/com/git/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World January 2018");
+		
+		System.out.println("Importing Project from GitHub");
 
 	}
 
